b3ccf00d102e99a7cb7127e5fcc1b2cd2de1758a
Abdurahim Miftah <abdugreat3@gmail.com>
2023-01-04 13:28:15 +0200

    Microverse JS Challege 1

---
+++ JavascriptChallenge1.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+    let jump = 0
+    for(let i=0;i<c.length;i++){
+        if(c[i+2]==0){
+            i++;
+            jump++;
+        }else if(c[i+1]==0) {
+            jump++;
+        }
+    }
+    return jump;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const c = readLine().replace(/\s+$/g, '').split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const result = jumpingOnClouds(c);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
